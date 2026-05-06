df01c4a68a2cdfe45ed76eced1698fbece2411e3
Eduardo Lucca <eduardo.lucca2011@gmail.com>
2026-05-06 10:46:52 -0300

    Delete Atividades directory

--- Atividades/Aula 18 - ex 3/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String a = "Curso de Java";
-        String b = "- Exemplo Sting";
-        String c = a+b;
-        System.out.println("Tamanho da String: "+c.length());
-    }
-}
